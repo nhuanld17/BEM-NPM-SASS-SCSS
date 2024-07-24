17af795a5ada8068da900d80b1acae48ebb27596
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-07-24 21:51:22 +0700

    Learn NPM

---
+++ 2. NPM/npm.js
@@ -0,0 +1,9 @@
+/* 
+    NPM là viết tắt của Node Package Manager. 
+    Là một công cụ tạo và quản lí các thư viện lập trình JavaScript cho Node.JS.
+    Trường hợp không sử dụng NPM, bạn sẽ cần tải toàn bộ các thư viện một cách thủ 
+    công, sau đó, bạn cần thực hiện nhúng thư viện vào chính dự án của mình. Điều 
+    này làm mất nhiều thời gian hơn để hoàn thành. Còn khi áp dụng NPM, bạn chỉ cần 
+    1 dòng lệnh hoàn tất công việc lưu thư viện. 
+    Link website: https://www.npmjs.com/ 
+*/
